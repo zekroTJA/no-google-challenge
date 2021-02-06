8dd0a5716e255aa73575e85514719d98e4c1cf0a
zekroTJA <riho@live.de>
2021-02-06 18:25:27 +0100

    add better error handling

--- WebApp/src/app/routes/login-route/login-route.component.ts
+++ WebApp/src/app/routes/login-route/login-route.component.ts
@@ -5,6 +5,7 @@ import {
   SnackBarType,
 } from 'src/app/components/snack-bar/snack-bar.service';
 import { IAPIService } from 'src/app/services/api.interface';
+import { ErrorService } from 'src/app/services/error.service';
 
 @Component({
   selector: 'app-login-route',
@@ -17,6 +18,7 @@ export class LoginRouteComponent implements OnInit {
   constructor(
     @Inject('APIService') private api: IAPIService,
     private router: Router,
+    private errors: ErrorService,
     private snackBar: SnackBarService
   ) {}
 
@@ -35,24 +37,24 @@ export class LoginRouteComponent implements OnInit {
   }
 
   async onLogin(loginName: string, password: string) {
-    try {
-      await this.api.login(loginName, password).toPromise();
-      this.router.navigate(['/']);
-    } catch (err) {
-      this.snackBar.show('Invalid username or password.', SnackBarType.ERROR);
-    }
+    this.errors.wrapRequest(
+      async () => {
+        await this.api.login(loginName, password).toPromise();
+        this.router.navigate(['/']);
+      },
+      () =>
+        this.snackBar.show('Invalid username or password.', SnackBarType.ERROR)
+    );
   }
 
   async onRegister(loginName: string, password: string) {
-    try {
+    this.errors.wrapRequest(async () => {
       await this.api.register(loginName, password).toPromise();
       this.snackBar.show(
         'Successfully registered. You will now being logged in.',
         SnackBarType.SUCCESS
       );
       await this.onLogin(loginName, password);
-    } catch (err) {
-      this.snackBar.show('Registration failed.', SnackBarType.ERROR);
-    }
+    });
   }
 }
---
+++ WebApp/src/app/services/error.service.ts
@@ -0,0 +1,39 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  SnackBarService,
+  SnackBarType,
+} from '../components/snack-bar/snack-bar.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ErrorService {
+  constructor(private snackBar: SnackBarService) {}
+
+  async wrapAsync<TResult, TError>(
+    func: () => Promise<TResult>,
+    onError?: (err: TError) => void
+  ): Promise<TResult> {
+    try {
+      return await func();
+    } catch (err) {
+      onError?.call(this, err);
+      return Promise.resolve((null as any) as TResult);
+    }
+  }
+
+  wrapRequest<T>(
+    req: () => Promise<T>,
+    onError?: (err: HttpErrorResponse) => void
+  ) {
+    if (!onError) onError = this.defaultRequestErrorHandler.bind(this);
+    return this.wrapAsync(() => req(), onError);
+  }
+
+  private defaultRequestErrorHandler(err: HttpErrorResponse) {
+    let content = 'An unknown error occured.';
+    if (err.error) content = `${err.error} (${err.status})`;
+    this.snackBar.show(content, SnackBarType.ERROR, 3500);
+  }
+}
